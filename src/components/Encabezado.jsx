750e5242da7d15aaeb76566cbbbde20ebf26618d
KennRod <rodriguezjosh2003@gmail.com>
2025-03-14 20:44:20 -0600

    prueba de logo

--- src/components/Encabezado.jsx
+++ src/components/Encabezado.jsx
@@ -1,100 +1,104 @@
-    import React, { useState } from "react";
-    import { useNavigate } from "react-router-dom";
-    import Container from "react-bootstrap/Container";
-    import Nav from "react-bootstrap/Nav";
-    import Navbar from "react-bootstrap/Navbar";
-    import Offcanvas from "react-bootstrap/Offcanvas";
-    import logo from "../assets/react.svg";
-    import { useAuth } from "../database/authcontext";
-    import 'bootstrap-icons/font/bootstrap-icons.css';
-    import "../App.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
-    const Encabezado = () => {
-    const [isCollapsed, setIsCollapsed] = useState(false);
-    const { isLoggedIn, logout } = useAuth();
-    const navigate = useNavigate();
+import { IoMdBookmarks } from "react-icons/io";
+import { useAuth } from "../database/authcontext";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "../App.css";
 
-    const handleLogout = async () => {
-        try {
-        // Cerrar el offcanvas antes de proceder
-        setIsCollapsed(false);
+const Encabezado = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const { isLoggedIn, logout } = useAuth();
+  const navigate = useNavigate();
 
-        // Eliminar variables almacenadas en localStorage
-        localStorage.removeItem("adminEmail");
-        localStorage.removeItem("adminPassword");
+  const handleLogout = async () => {
+    try {
+      setIsCollapsed(false);
 
-        // Cerrar sesión
-        await logout();
+      localStorage.removeItem("adminEmail");
+      localStorage.removeItem("adminPassword");
 
-        // Redirigir al inicio
-        navigate("/");
-        } catch (error) {
-        console.error("Error al cerrar sesión:", error);
-        }
-    };
+      await logout();
+      navigate("/");
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error);
+    }
+  };
 
-    const handleToggle = () => setIsCollapsed(!isCollapsed);
+  const handleToggle = () => setIsCollapsed(!isCollapsed);
+  const handleNavigate = (path) => {
+    navigate(path);
+    setIsCollapsed(false);
+  };
 
-    // Funciones de navegación
-    const handleNavigate = (path) => {
-        navigate(path);
-        setIsCollapsed(false);
-    };
-
-    return (
-        <Navbar expand="sm" fixed="top" className="color-navbar">
-        <Container>
-            <Navbar.Brand onClick={() => handleNavigate("/inicio")} className="text-white" style={{ cursor: "pointer" }}>
-            <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />{" "}
-            <strong>EduNova</strong>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="offcanvasNavbar-expand-sm" onClick={handleToggle} />
-            <Navbar.Offcanvas
-            id="offcanvasNavbar-expand-sm"
-            aria-labelledby="offcanvasNavbarLabel-expand-sm"
-            placement="end"
-            show={isCollapsed}
-            onHide={() => setIsCollapsed(false)}
+  return (
+    <Navbar expand="sm" fixed="top" className="color-navbar">
+      <Container>
+        <Navbar.Brand
+          onClick={() => handleNavigate("/inicio")}
+          className="text-white"
+          style={{ cursor: "pointer" }}
+        >
+          <IoMdBookmarks size={30} color="white" className="me-2" />
+          <strong>EduNova</strong>
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="offcanvasNavbar-expand-sm"
+          onClick={handleToggle}
+        />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar-expand-sm"
+          aria-labelledby="offcanvasNavbarLabel-expand-sm"
+          placement="end"
+          show={isCollapsed}
+          onHide={() => setIsCollapsed(false)}
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title
+              id="offcanvasNavbarLabel-expand-sm"
+              className={isCollapsed ? "color-texto-marca" : "text-white"}
             >
-            <Offcanvas.Header closeButton>
-                <Offcanvas.Title
-                id="offcanvasNavbarLabel-expand-sm"
+              Menú
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link
+                onClick={() => handleNavigate("/inicio")}
                 className={isCollapsed ? "color-texto-marca" : "text-white"}
-                >
-                Menú
-                </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-
+              >
+                {isCollapsed ? (
+                  <i className="bi-house-door-fill me-2"></i>
+                ) : null}
+                <strong>Inicio</strong>
+              </Nav.Link>
+              {isLoggedIn ? (
                 <Nav.Link
-                    onClick={() => handleNavigate("/inicio")}
-                    className={isCollapsed ? "color-texto-marca" : "text-white"}
+                  onClick={handleLogout}
+                  className={isCollapsed ? "text-black" : "text-white"}
                 >
-                    {isCollapsed ? <i className="bi-house-door-fill me-2"></i> : null}
-                    <strong>Inicio</strong>
+                  Cerrar Sesión
                 </Nav.Link>
-                {isLoggedIn ? (
-                    <>
-                    <Nav.Link onClick={handleLogout} className={isCollapsed ? "text-black" : "text-white"}>
-                        Cerrar Sesión
-                    </Nav.Link>
-                    </>
-                ) : location.pathname === "/" && (
-                    <Nav.Link
+              ) : (
+                location.pathname === "/" && (
+                  <Nav.Link
                     onClick={() => handleNavigate("/")}
                     className={isCollapsed ? "text-black" : "text-white"}
-                    >
+                  >
                     Iniciar Sesión
-                    </Nav.Link>
-                )}
+                  </Nav.Link>
+                )
+              )}
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
+};
 
-                </Nav>
-            </Offcanvas.Body>
-            </Navbar.Offcanvas>
-        </Container>
-        </Navbar>
-    );
-    };
-
-    export default Encabezado;
+export default Encabezado;
